43b3dae6018b0e901140264562364226b81d24eb
dit02 <dit@imac-7.local>
2019-03-19 15:33:06 +0900

    toggle button

--- ChangeColor/ViewController.swift
+++ ChangeColor/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var myLabel: UILabel!
+    var colorState = true
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func buttonPressen(_ sender: Any) {
+        myLabel.text = "Button Pressed!!!"
+    }
+    
+    @IBAction func Init(_ sender: Any) {
+        myLabel.text = ""
+    }
+    
+    @IBAction func changeColor(_ sender: Any) {
+        if colorState == true {
+        self.view.backgroundColor = UIColor.yellow
+        colorState = false
+        }
+        else if colorState == false{
+            self.view.backgroundColor = UIColor.white
+            colorState = true
+        }
+    }
+    
 }
 
